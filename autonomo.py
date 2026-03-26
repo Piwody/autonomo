a8b862760cdf803ded345be4fcc6e11913cef5d2
Piwody <mateosteve1312@gmail.com>
2026-03-26 17:41:11 -0500

    final proyecto

---
+++ autonomo.py
@@ -0,0 +1,63 @@
+import time
+
+
+intentos = 0
+
+
+
+def prueba(prefijo, longitud, alfabeto, objetivo):
+    global intentos
+    
+  
+    if len(prefijo) == longitud:
+        intentos += 1
+        if prefijo == objetivo:
+            return prefijo
+        return None
+
+    
+    for char in alfabeto:
+        resultado = prueba(prefijo + char, longitud, alfabeto, objetivo)
+
+        if resultado: 
+            return resultado
+    
+    return None
+
+
+# Esta función realiza un ataque de fuerza bruta para encontrar la contraseña dada un alfabeto y una contraseña objetivo.
+
+
+def bruteforce(alfabeto, password):
+    global intentos
+    intentos = 0
+    tiempo = time.time()
+    
+   
+    
+    
+    for largo in range(1, 10): 
+
+        print("viendo los caracteres " + str(largo) + "...")
+        encontrada = prueba("", largo, alfabeto, password)
+        
+        if encontrada:
+
+            final = time.time()
+            tiempo_total = final - tiempo
+           
+            print("te cachamos tu pass es: " + str(encontrada))
+            print("intentos: " + str(intentos))
+            print("tiempo en segundos: " + str((tiempo_total)) )
+            
+            return encontrada
+
+    print("No se encontró la contraseña con el alfabeto.")
+    return None
+
+
+
+caracterdeuser= "abcdefghijklmnopqrstuvwxyz0123456789"
+contra = "carro" 
+
+bruteforce(caracterdeuser, contra)

